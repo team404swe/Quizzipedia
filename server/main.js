96858677fba281107a8141912a5507316dfe915a
andreamultineddu <andrea.multineddu@gmail.com>
2016-07-09 15:59:16 +0200

    update main server

--- server/main.js
+++ server/main.js
@@ -1,7 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+import '../imports/publishers/questionPublisher.js';
+import '../imports/publishers/quizPublisher.js';
+import '../imports/statistic/Statistics.js';
 import '../imports/api/questions.js';
-//import '../imports/publishers/questionPublisher.js';
-//import '../imports/publishers/quizPublisher.js';
 
 Meteor.startup(() => {
   // code to run on server at startup

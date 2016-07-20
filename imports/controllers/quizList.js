30840de4dd2cbaca20509b5bc23eb2e952e105de
tinovad <tinovad@hotmail.com>
2016-07-20 23:57:29 +0200

    Da completare: ricupero domande Quiz

--- imports/controllers/quizList.js
+++ imports/controllers/quizList.js
@@ -3,6 +3,8 @@ import angularMeteor from 'angular-meteor';
 import template from '../templates/quizList.html';
 
 import { Quizzes } from '../publishers/quizPublisher.js';
+import { Questions } from '../publishers/questionPublisher.js';
+import checkAnswer from '../parser/Parser.js'
 
 class QuizListController{
 	constructor($scope) {
@@ -22,11 +24,23 @@ class QuizListController{
 				return Quizzes.find({}, {"sort" : [['createdAt', 'desc']]});
 			}
 		});
+		this.subscribe('questions');
+	
+		this.helpers({
+			questions() {
+				return Questions.find({}, {"sort" : [['createdAt', 'desc']]});
+			}					
+		});	
 	}
 	
 	setQuiz(qid){
 		var tempQuiz = Quizzes.find({_id: qid}).fetch();
 		quizComp = tempQuiz[0];
+		for (var i = 0; i< quizComp.questions.length; i++ )
+		{ var quest =Questions.find( { _id: quizComp.questions[i]}).fetch();
+			quizComp.questions[i] = undefined;
+			quizComp.questions[i] = checkAnswer(quest[0][0].QMLtext);
+		}
 		
 	}
 }

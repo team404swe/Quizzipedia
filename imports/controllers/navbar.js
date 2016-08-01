70ae2614ee168c53774c3cec30ed787ada66262c
tinovad <tinovad@hotmail.com>
2016-08-01 23:23:04 +0200

    Ci siamo quasi... il menu viene fuori
    Ma servono più click per togliere l'ombra

--- imports/controllers/navbar.js
+++ imports/controllers/navbar.js
@@ -11,6 +11,18 @@ class NavbarController {
 				return !!Meteor.userId();
 			}
 		});
+		this.slideMenu();
+		
+		$('.collapsible').collapsible();
+	}
+	
+	slideMenu(){
+		$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+   
 	}
 }
 

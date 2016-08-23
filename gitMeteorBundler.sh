eeb4df3708266aab9376185f85c5c0c0bf3be40a
DBortot <davide.bortot94@gmail.com>
2016-08-23 08:59:44 +0200

    screenshot

--- gitMeteorBundler.sh
+++
@@ -1,9 +0,0 @@
-echo Building Meteor-app in OpenShift repo
-
-meteor build ../quizzipedia_OS --directory --server-only
-echo Successfully builded
-
-cd ../quizzipedia_OS
-git add --all 
-git commit -m "New Meteor Deployment"
-git push
